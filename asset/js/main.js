402c8e44ab77816f35ceb2264913cadce5378d62
priyanshu727 <priyanshumishra7272@email.com>
2023-04-07 15:04:56 +0530

    my first project

---
+++ asset/js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        576:{
+            items:2,
+        },
+        992:{
+            items:3,
+        }
+    }
+    
+})
